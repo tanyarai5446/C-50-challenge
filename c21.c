ed6ef79c5228aeb55276083faf8dd330e4f0bd03
tanyarai5446 <tr5335074@gmail.com>
2025-10-22 15:56:57 +0530

    string

---
+++ c21.c
@@ -0,0 +1,25 @@
+//Print all permutations of a string
+#include <stdio.h>
+void swap(char *x, char *y) {
+    char temp = *x;
+    *x = *y;
+    *y = temp;
+    
+}
+void permute(char *a, int l, int r) {
+    if (l == r) {
+        printf("%s\n", a);
+    } else {
+        for (int i = l; i <= r; i++) {
+            swap((a + l), (a + i));
+            permute(a, l + 1, r);
+            swap((a + l), (a + i)); // backtrack
+        }
+    }
+}
+int main() {
+    char str[] = "tanya";
+    int n = 5; // Length of the string
+    permute(str, 0, n - 1);
+    return 0;
+}
