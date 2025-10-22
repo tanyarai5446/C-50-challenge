9947627408e5d8f279190e9a2963ac3be1b42362
tanyarai5446 <tr5335074@gmail.com>
2025-10-22 15:05:45 +0530

    array easy

--- c19.c
+++ c19.c
@@ -1,8 +1,8 @@
 // Merge two sorted arrays without using extra space
 #include <stdio.h>
 int main() {
-    int arr1[] = {1, 3, 5, 0};
-    int arr2[] = {2, 6, 0, 8};
+    int arr1[] = {1, 3, 5, 9};
+    int arr2[] = {2, 4, 6, 8};
     int n1 = sizeof(arr1) / sizeof(arr1[0]);
     int n2 = sizeof(arr2) / sizeof(arr2[0]);
     
