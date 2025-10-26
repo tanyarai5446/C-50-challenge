a36f4d46ff9164e1dbda6b508b47facb6880b134
tanyarai5446 <tr5335074@gmail.com>
2025-10-26 22:31:57 +0530

    string

---
+++ c26.c
@@ -0,0 +1,18 @@
+//Reverse a string
+#include <stdio.h>
+#include <string.h>
+int reverseStr(){
+    char str[] = "Hello, World!";
+    int n = strlen(str);
+    for (int i = 0; i < n / 2; i++) {
+        char temp = str[i];
+        str[i] = str[n - i - 1];
+        str[n - i - 1] = temp;
+    }
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
+int main() {
+    reverseStr();
+    return 0;
+}
