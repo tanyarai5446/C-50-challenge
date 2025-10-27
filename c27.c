40ca9395bd485e0d9c810b842878eadb15b24df8
tanyarai5446 <tr5335074@gmail.com>
2025-10-27 23:45:42 +0530

    string

---
+++ c27.c
@@ -0,0 +1,37 @@
+//Longest common prefix
+#include<stdio.h>
+#include<string.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_WORDS 10
+#define MAX_LENGTH 100
+
+int main() {
+    // Input words
+    char words[MAX_WORDS][MAX_LENGTH] = {
+        "flower",
+        "flow",
+        "flowing"
+    };
+    int size = 3;
+
+    char prefix[MAX_LENGTH];
+    int i, j;
+
+    // Start with the first word as prefix
+    strcpy(prefix, words[0]);
+
+    for (i = 1; i < size; i++) {
+        // Compare prefix with current word
+        for (j = 0; j < strlen(prefix); j++) {
+            if (prefix[j] != words[i][j]) {
+                break; // mismatch found
+            }
+        }
+        prefix[j] = '\0'; // cut the prefix at mismatch
+    }
+
+    printf("Longest Common Prefix: %s\n", prefix);
+    return 0;
+}
