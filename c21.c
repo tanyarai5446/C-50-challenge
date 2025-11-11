8ad66943fb6bae2e6e6e8d449397f700b4ddf2bc
tanyarai5446 <tr5335074@gmail.com>
2025-11-11 22:07:38 +0530

    .

--- c21.c
+++ c21.c
@@ -7,17 +7,22 @@ void swap(char *x, char *y) {
     
 }
 void permute(char *a, int l, int r) {
-    if (l == r) {
+    if (l == r) 
+    {
         printf("%s\n", a);
-    } else {
-        for (int i = l; i <= r; i++) {
+    } 
+    else 
+    {
+        for (int i = l; i <= r; i++) 
+        {
             swap((a + l), (a + i));
             permute(a, l + 1, r);
             swap((a + l), (a + i)); // backtrack
         }
     }
 }
-int main() {
+int main()
+{
     char str[] = "tanya";
     int n = 5; // Length of the string
     permute(str, 0, n - 1);
