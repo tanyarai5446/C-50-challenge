33691267fd87258fef098d8b7c292adc4b224974
tanyarai5446 <tr5335074@gmail.com>
2025-12-07 23:16:06 +0530

    sort an array of 0s,1s and 2s

--- c04.c
+++ c04.c
@@ -1,6 +1,7 @@
 //4. Sort an array of 0s, 1s and 2s (Dutch National Flag Problem)
 #include<stdio.h>
-void sort0s1s2s(int a[],int n){
+void sort0s1s2s(int a[],int n)
+{
     int i=0,m=0,j=n-1;
     while(m<=j){
         if(a[m]==0)
@@ -30,5 +31,4 @@ int main(){
     int a[]={1,0,1,1,2,0,0,2};
     int n=sizeof(a)/sizeof(a[0]);
     sort0s1s2s(a,n);
-
 }
