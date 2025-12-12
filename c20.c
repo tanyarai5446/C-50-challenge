7eaab380967f667c33685283de1fc460ea0d15a4
tanyarai5446 <tr5335074@gmail.com>
2025-12-12 23:18:47 +0530

    max product subarray

--- c20.c
+++ c20.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <stdio.h>
 
-int max(int a, int b) {
+int max(int a, int b) 
+{
     return (a > b) ? a : b;
 }
 
-int min(int a, int b) {
+int min(int a, int b) 
+{
     return (a < b) ? a : b;
 }
 
