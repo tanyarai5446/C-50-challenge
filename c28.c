9e9f4ccb987c45962da3fe5397037e2ebb1eb38f
tanyarai5446 <tr5335074@gmail.com>
2025-12-14 23:11:33 +0530

    myatoi

--- c28.c
+++ c28.c
@@ -8,11 +8,15 @@ int myAtoi(const char *s)
     while(s[i]==' ')//skip spaces
     i++;
     while(s[i]=='-'||s[i]=='+')//ensuring sign
-    {sign=(s[i]=='-')?-1:1;
-    i++;}
+    {
+        sign=(s[i]=='-')?-1:1;
+        i++;
+    }
     while(s[i]>='0'&&s[i]<='9')
-    {result=result*10+(s[i]-'0');//doesnt converts char to int and for char like '1' we use s[i]-'0'i.e. 56-55=1;
-    i++;}
+    {
+        result=result*10+(s[i]-'0');//doesnt converts char to int and for char like '1' we use s[i]-'0'i.e. 56-55=1;
+        i++;
+    }
     return sign*result;
 
 }
