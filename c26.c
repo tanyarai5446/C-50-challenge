1c1ef28d546c67c68d61dbf9572eca418dd6e363
tanyarai5446 <tr5335074@gmail.com>
2025-12-14 23:09:01 +0530

    reverse a string

--- c26.c
+++ c26.c
@@ -1,10 +1,12 @@
 //Reverse a string
 #include <stdio.h>
 #include <string.h>
-int reverseStr(){
+int reverseStr()
+{
     char str[] = "Hello, World!";
     int n = strlen(str);
-    for (int i = 0; i < n / 2; i++) {
+    for (int i = 0; i < n / 2; i++) 
+    {
         char temp = str[i];
         str[i] = str[n - i - 1];
         str[n - i - 1] = temp;
@@ -12,7 +14,8 @@ int reverseStr(){
     printf("Reversed string: %s\n", str);
     return 0;
 }
-int main() {
+int main() 
+{
     reverseStr();
     return 0;
 }
